506c3d39540e5e24671f2d6eda883b6bd591243e
ExLog <55097092+ExLog@users.noreply.github.com>
2021-05-15 02:27:24 +0800

    feat: fetch to API and display images

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app dark>
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
     </v-app-bar>
@@ -16,24 +16,6 @@
 export default {
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
       title: 'Vuetify.js',
     }
   },
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,105 @@
 <template>
-  <v-row> </v-row>
+  <div>
+    <v-card flat class="py-12">
+      <v-card-text>
+        <v-row no-gutters>
+          <v-col v-for="filter in filters.sfw" :key="filter" cols="auto">
+            <v-btn-toggle v-model="filtered">
+              <v-btn :value="filter">
+                {{ filter }}
+              </v-btn>
+            </v-btn-toggle>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+
+    <v-row>
+      <v-col v-for="image in images" :key="image" cols="4">
+        <v-img :src="image" aspect-ratio="1" class="grey" position="top">
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="#000"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      images: [],
+      filters: [{ sfw: [] }],
+      filtered: 'waifu',
+    }
+  },
+  methods: {
+    filterImages(image) {
+      if (!this.images.includes(image)) this.images.push(image)
+    },
+
+    async getImages() {
+      const res = await this.$axios({
+        method: 'POST',
+        baseURL: 'https://api.waifu.pics',
+        url: `/many/sfw/${this.filtered}`,
+        data: {
+          exclude: [],
+        },
+      })
+      if (this.filtered) this.images = []
+      res.data.files.forEach((image) => this.filterImages(image))
+    },
+
+    getNextImages() {
+      window.onscroll = async () => {
+        const bottomOfWindow =
+          document.documentElement.scrollTop + window.innerHeight ===
+          document.documentElement.offsetHeight
+        if (bottomOfWindow) {
+          const res = await this.$axios({
+            method: 'POST',
+            baseURL: 'https://api.waifu.pics',
+            url: `/many/sfw/${this.filtered}`,
+            data: {
+              exclude: [],
+            },
+          })
+          res.data.files.forEach((image) => this.filterImages(image))
+        }
+      }
+    },
+
+    async getFilters() {
+      const res = await this.$axios({
+        method: 'GET',
+        baseURL: 'https://api.waifu.pics',
+        url: '/endpoints',
+      })
+
+      // Prevent duplicates
+      this.filters.sfw = [...new Set(res.data.sfw)]
+    },
+  },
+
+  created() {
+    this.getImages()
+    this.getFilters()
+  },
+  mounted() {
+    this.getNextImages()
+  },
+  watch: {
+    filtered() {
+      this.getPictures(this.filtered)
+    },
+  },
+}
 </script>
